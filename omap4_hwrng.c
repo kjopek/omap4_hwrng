de152d9d4548e7da017041ef8813075208077a75
_kjopek <kjopek@gmail.com>
2015-04-16 23:32:14 +0200

    initial commit

---
+++ omap4_hwrng.c
@@ -0,0 +1,97 @@
+#include <sys/param.h>
+#include <sys/kernel.h>
+#include <sys/lock.h>
+#include <sys/malloc.h>
+#include <sys/module.h>
+#include <sys/random.h>
+#include <sys/selfinfo.h>
+#include <sys/systm.h>
+
+#include <dev/randomdev/randomdev.h>
+#include <dev/randomdev/randomdev_soft.h>
+#include <dev/randomdev/random_harvestq.h>
+#include <dev/randomdev/live_entropy_sources.h>
+#include <dev/randomdev/random_adaptors.h>
+
+#include "OMAP4_hwrng.h"
+
+// TODO: Add RANDOM_PURE_OMAP4 to /sys/random.h
+
+static struct random_hardware_source random_omap4_rng = {
+	.ident = "OMAP4 Hardware RNG",
+	.source = RANDOM_PURE_OMAP4,
+	.read = omap4_hwrng_read
+}; 
+
+static struct omap4_hwrng_softc *softc = NULL;
+
+static int
+omap4_hwrng_init(void)
+{
+
+	return (0);	
+}
+
+static int
+omap4_hwrng_probe(device_t dev)
+{
+	if (!ofw_bus_status_okay(dev))
+		return (ENXIO);
+
+	if (!ofw_bus_is_compatible(dev, "ti,omap4-rng"))
+		return (ENXIO);
+
+	device_set_desc(dev, "omap4 Hardware RNG");
+
+	return (BUS_PROBE_DEFAULT);
+}
+
+static int
+omap4_hwrng_attach(device_t dev)
+{
+	int rid;
+	struct omap4_hwrng_softc *sc;
+
+	if (softc != NULL) {
+		return (ENXIO);
+	}
+
+	sc = softc = device_get_softc(dev);
+	sc->sc_dev = dev;
+	rid = 0;
+	sc->sc_mem_res = bus_alloc_resource_any(dev, SYS_RES_MEMORY, &rid,
+	    RF_ACTIVE);
+	if (!sc->sc_mem_res) {
+		device_printf(dev, "cannot allocate memory resources\n");
+		return (ENXIO);
+	}
+
+	
+
+}
+
+static int
+omap4_hwrng_modevent(module_t mod, int type, void *unused)
+{
+	int error = 0;
+	switch (type) {
+	case MOD_LOAD:
+		error = omap4_hwrng_probe();
+		if (!error) {
+			live_entropy_source_register(&random_omap4_rng);
+			omap4_hwrng_init();
+		}
+		break;
+
+	case MOD_UNLOAD:
+		live_entropy_source_deregister(&random_omap4_rng);
+		break;
+
+	default:
+		break;
+	}
+
+	return (error);
+}
+
+LIVE_ENTROPY_SRC_MODULE(omap4_hwrng, omap4_hwrng_modevent, 1);
---
+++ omap4_hwrng.h
@@ -0,0 +1,26 @@
+#ifndef __OMAP4_HWRNG_H__
+#define __OMAP4_HWRNG_H__
+
+#define OMAP4_HWRNG_OUTPUT_L	(0x00)
+#define OMAP4_HWRNG_OUTPUT_H	(0x04)
+#define OMAP4_HWRNG_STATUS	(0x08)
+#define OMAP4_HWRNG_INTMASK	(0x0C)
+#define OMAP4_HWRNG_INTACK	(0x10)
+#define OMAP4_HWRNG_CONTROL	(0x14)
+#define OMAP4_HWRNG_CONFIG	(0x18)
+#define OMAP4_HWRNG_ALARMCNT	(0x1c)
+#define OMAP4_HWRNG_FROENABLE	(0x20)
+#define OMAP4_HWRNG_FRODETUNE	(0x24)
+#define OMAP4_HWRNG_ALARMMASK	(0x28)
+#define OMAP4_HWRNG_ALARMSTOP	(0x2C)
+#define OMAP4_HWRNG_REV		(0x1FE0)
+#define OMAP4_HWRNG_SYSCONFIG	(0x1FE4)
+
+struct omap_hwrng_softc {
+	device_t	sc_dev;
+	struct mtx	sc_mtx;
+	struct resource	sc_irq_res;
+	struct resource sc_mem_res;
+};
+
+#endif
